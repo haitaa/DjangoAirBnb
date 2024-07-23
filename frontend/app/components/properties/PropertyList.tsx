eb719fef06a576e25d52dab1ed96a4dfc4e6284f
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-23 20:06:17 +0300

    Property List (static)

---
+++ frontend/app/components/properties/PropertyList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PropertyListItem from "./PropertyListItem";
+
+export default function PropertyList() {
+    return (
+        <>
+            <PropertyListItem />
+            <PropertyListItem />
+            <PropertyListItem />
+        </>
+    );
+}
