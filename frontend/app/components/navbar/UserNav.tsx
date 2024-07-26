92806c14e628f5d16309d8a5d8bc070a001e4dfd
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-26 17:42:44 +0300

    User menu popup

--- frontend/app/components/navbar/UserNav.tsx
+++ frontend/app/components/navbar/UserNav.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+
+import MenuLink from "./MenuLink";
 
 function UserNav() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className="p-2 relative inline-block border rounded-full">
-            <button className="flex items-center">
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center"
+            >
                 <svg
                     fill="none"
                     viewBox="0 0 24 24"
@@ -32,6 +41,19 @@ function UserNav() {
                     />
                 </svg>
             </button>
+
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+                    <MenuLink
+                        onClick={() => console.log("Clicked button")}
+                        label="Log in"
+                    />
+                    <MenuLink
+                        onClick={() => console.log("Clicked button")}
+                        label="Sign up"
+                    />
+                </div>
+            )}
         </div>
     );
 }
