dee6952ddb4f8787232e0c324421df1a71a0d0e4
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-26 14:47:14 +0300

    Landlord Detail Page component

---
+++ frontend/app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ContactButton() {
+    return (
+        <div className="mt-6 py-4 px-6 bg-airbnb hover:bg-airbnb-dark text-white rounded-xl transition">
+            Contact
+        </div>
+    );
+}
