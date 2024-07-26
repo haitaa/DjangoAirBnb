95502622864204cc1bfab26f7a3c5f4d22785a8f
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-26 16:02:00 +0300

    Inbox component

---
+++ frontend/app/components/inbox/Conversation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Conversation() {
+    return (
+        <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
+            <p className="mb-6 text-xl">John Doe</p>
+            <p className="text-airbnb-dark">Go to conversation</p>
+        </div>
+    );
+}
+
+export default Conversation;
