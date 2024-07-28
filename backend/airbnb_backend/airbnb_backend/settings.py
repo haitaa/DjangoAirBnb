413ca6f884d70e9e8f873958f0bb8d9c0946457c
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-28 19:14:48 +0300

    Showing properties

--- backend/airbnb_backend/airbnb_backend/settings.py
+++ backend/airbnb_backend/airbnb_backend/settings.py
@@ -50,9 +50,11 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://localhost:3000",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_AUTH = {
     "USE_JWT": True,
--- frontend/app/components/properties/PropertyList.tsx
+++ frontend/app/components/properties/PropertyList.tsx
@@ -1,13 +1,42 @@
-import React from "react";
+"use client";
 
+import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
 
+export type PropertyType = {
+    id: string;
+    title: string;
+    price_per_night: number;
+    image_url: string;
+};
+
 export default function PropertyList() {
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+
+    const getProperties = async function () {
+        const url = "http://localhost:8000/api/properties/";
+
+        await fetch(url, {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                setProperties(json.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    useEffect(() => {
+        getProperties();
+    }, []);
+
     return (
         <>
-            <PropertyListItem />
-            <PropertyListItem />
-            <PropertyListItem />
+            {properties.map((property) => (
+                <PropertyListItem key={property.id} property={property} />
+            ))}
         </>
     );
 }
