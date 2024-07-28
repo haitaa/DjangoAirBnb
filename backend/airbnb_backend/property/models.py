80ccab3fc4b67131944369d544c6541cc7a994cb
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-28 18:08:40 +0300

    Property app

--- backend/airbnb_backend/property/models.py
+++ backend/airbnb_backend/property/models.py
@@ -1,3 +1,20 @@
+import uuid
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from useraccount.models import User
+
+class Property(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price_per_night = models.IntegerField()
+    bedrooms = models.IntegerField()
+    guests = models.IntegerField()
+    country = models.CharField(max_length=255)
+    country_code = models.CharField(max_length=10)
+    category = models.CharField(max_length=255)
+
+    image = models.ImageField(upload_to='uploads/properties')
+    landlord = models.ForeignKey(User, related_name='properties', on_delete=models.CASCADE)
+    created_t = models.DateTimeField(auto_now_add=True)
