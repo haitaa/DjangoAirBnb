fd20531ddfd66635a1ba52226aec4a7e78ca6567
Mustafa Haita <mustafahaitaa@gmail.com>
2024-08-03 17:46:33 +0300

    Detail page for properties

--- frontend/app/components/properties/ReservationSideBar.tsx
+++ frontend/app/components/properties/ReservationSideBar.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
-export default function ReservationSideBar() {
+export type Property = {
+    id: string;
+    price_per_night: number;
+};
+
+interface ReservationSideBarProps {
+    property: Property;
+}
+
+const ReservationSideBar: React.FC<ReservationSideBarProps> = ({
+    property,
+}) => {
     return (
         <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
-            <h2 className="mb-5 text-2xl">$200 per night</h2>
+            <h2 className="mb-5 text-2xl">
+                ${property.price_per_night} per night
+            </h2>
 
             <div className="mb-6 p-3 border border-gray-400 rounded-xl">
                 <label className="mb-2 block font-bold text-xs">Guests</label>
@@ -36,4 +49,6 @@ export default function ReservationSideBar() {
             </div>
         </aside>
     );
-}
+};
+
+export default ReservationSideBar;
--- frontend/app/properties/[id]/page.tsx
+++ frontend/app/properties/[id]/page.tsx
@@ -1,58 +1,76 @@
 import Image from "next/image";
+
 import ReservationSideBar from "@/app/components/properties/ReservationSideBar";
+import apiService from "@/app/services/apiService";
 
-const PropertyDetailPage = () => {
-    return (
-        <main className="max-w-[1500px] mx-auto px-6 pb-6">
-            <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
-                <Image
-                    fill
-                    src="/beach_1.jpg"
-                    alt="Beach House"
-                    className="object-cover w-full h-full"
-                />
-            </div>
+const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
+    try {
+        const url = `/api/properties/${params.id}`;
+        const property = await apiService.get(url);
 
-            <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
-                <div className="py-6 pr-6 col-span-3">
-                    <h1 className="mb-4 text-4xl">Property name</h1>
+        if (!property) {
+            return (
+                <main className="max-w-[1500px] mx-auto px-6 pb-6">
+                    <p>Property not found.</p>
+                </main>
+            );
+        }
 
-                    <span className="mb-6 block text-lg text-gray-600">
-                        4 guests - 2 bedrooms - 1 bathroom
-                    </span>
-
-                    <hr />
-
-                    <div className="py-6 flex items-center space-x-4">
-                        <Image
-                            src="/profile_pic_1.jpg"
-                            alt="The user name"
-                            width={50}
-                            height={50}
-                            className="rounded-full"
-                        />
-                        <p>
-                            <strong>John Doe</strong> is your host
-                        </p>
-                    </div>
-
-                    <hr />
-
-                    <p className="mt-6 text-lg">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Placeat quisquam autem ipsam eos dignissimos,
-                        corporis veritatis, minus nesciunt, molestias nihil
-                        aspernatur amet ad ea veniam incidunt voluptates ipsa
-                        deserunt doloribus praesentium accusamus quam quas qui
-                        debitis? Ducimus et voluptates nihil earum cumque facere
-                        provident quisquam, quo expedita, inventore magni id.
-                    </p>
+        return (
+            <main className="max-w-[1500px] mx-auto px-6 pb-6">
+                <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+                    <Image
+                        fill
+                        src={property.image_url}
+                        alt="Beach House"
+                        className="object-cover w-full h-full"
+                    />
                 </div>
 
-                <ReservationSideBar></ReservationSideBar>
-            </div>
-        </main>
-    );
+                <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+                    <div className="py-6 pr-6 col-span-3">
+                        <h1 className="mb-4 text-4xl">{property.title}</h1>
+
+                        <span className="mb-6 block text-lg text-gray-600">
+                            {property.guests} guests - {property.bedrooms}{" "}
+                            bedrooms - {property.bathrooms} bathroom
+                        </span>
+
+                        <hr />
+
+                        <div className="py-6 flex items-center space-x-4">
+                            <Image
+                                src={property.landlord.avatar_url}
+                                alt="The user name"
+                                width={50}
+                                height={50}
+                                className="rounded-full"
+                            />
+                            <p>
+                                <strong>{property.landlord.name}</strong> is
+                                your host
+                            </p>
+                        </div>
+
+                        <hr />
+
+                        <p className="mt-6 text-lg">{property.description}</p>
+                    </div>
+
+                    <ReservationSideBar
+                        property={property}
+                    ></ReservationSideBar>
+                </div>
+            </main>
+        );
+    } catch (error) {
+        console.error("Error fetching property details:", error);
+        return (
+            <main className="max-w-[1500px] mx-auto px-6 pb-6 pt-6">
+                <p>Failed to load property details. Please try again later.</p>
+            </main>
+        );
+    }
 };
 
 export default PropertyDetailPage;
