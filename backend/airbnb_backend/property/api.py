a6f7e549072ecc51cec269900b0aae6859d4d54d
Mustafa Haita <mustafahaitaa@gmail.com>
2024-08-07 19:55:59 +0300

    API endpoint

--- backend/airbnb_backend/property/api.py
+++ backend/airbnb_backend/property/api.py
@@ -26,11 +26,44 @@ def properties_list(request):
     is_favorites = request.GET.get('is_favorites', '')
     landlord_id = request.GET.get('landlord_id', '')
 
+    country = request.GET.get('country', '')
+    category = request.GET.get('category', '')
+    checkin_date = request.GET.get('checkIn', '')
+    checkout_date = request.GET.get('checkOut', '')
+    bedrooms = request.GET.get('numBedrooms', '')
+    bathrooms = request.GET.get('numBathrooms', '')
+    guests = request.GET.get('numGuests', '')
+
+    if checkin_date and checkout_date:
+        exact_matches = Reservation.objects.filter(start_date=checkin_date) | Reservation.objects.filter(end_date=checkout_date)
+        overlap_matches = Reservation.objects.filter(start_date__lte=checkout_date, end_date__lte=checkin_date)
+        all_matches = []
+
+        for reservation in exact_matches | overlap_matches:
+            all_matches.append(reservation.property_id)
+
+        properties = properties.exclude(id__in=all_matches)
+
     if landlord_id:
         properties = properties.filter(landlord_id=landlord_id)
 
     if is_favorites:
         properties = properties.filter(favorited__in=[user])
+
+    if country:
+        country = properties.filter(country=country)
+    
+    if guests:
+        guests = properties.filter(guest__gte=guests)
+
+    if bedrooms:
+        bedrooms = properties.filter(bedrooms__gte=bedrooms)
+
+    if bathrooms:
+        bathrooms = properties.filter(bathrooms__gte=bathrooms)
+
+    if category and category != 'undefined':
+        category = properties.filter(category=category)
 
     # Favorites
     if user:
